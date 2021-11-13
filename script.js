9fa2869e260993b632b5118ddbc90b3606cc5c85
OusJasseh <ousman16@hotmail.com>
2021-11-13 18:17:27 +0100

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-import { validateEmail, validateLength, comparePassword } from "./validate.js";
 
 
+document.getElementById("firstName").addEventListener("keyup", (event) => validateLength(event, "First Name"));
 
-document.getElementById("firstName").addEventListener("keyup", function (e) {
-    validateLength(e.target.value);
-});
 
-document.getElementById("lastName").addEventListener("keyup", function (e) {
-  validateLength(e.target.value);
-});
+document.getElementById("lastName").addEventListener("keyup", (event) => validateLength(event, "Last Name"));
 
-document.getElementById("email").addEventListener("keyup", function (e) {
-  validateLength(e.target.value);
-});
 
-document.getElementById("password").addEventListener("keyup", function (e) {
-  validateLength(e.target);
-});
+//document.getElementById("email").addEventListener("keyup", function (event)  validateLength(e.target.value));
 
-document.getElementById("confirmPassword").addEventListener("keyup", function (e) {
-  validateLength(e.target);
-});
+
+document.getElementById("password").addEventListener("keyup", (event) => validatepasswordLength(event, "Password"));
+
+
+document.getElementById("confirmPassword").addEventListener("keyup", (event) => confirmPassword(event.target.value)) "Confirm Password");
+
 
 document.getElementById("dateOfBirt").addEventListener("keyup", function (e) {
   validateLength(e.target);
@@ -31,10 +24,15 @@ document.getElementById("address").addEventListener("keyup", function (e) {
 });
 
 
+var forms = document.querySelectorAll(".needs-validation");
+
+
+
+
 document.getElementById("ValidationForm").addEventListener("submit", function(e) {
     e.preventDefault()
 
-    for (let element of e.target)   
+    for (let element of e.target.value)   
             switch(element.type) {
                 case 'text': 
                     validateLength(element)
@@ -51,8 +49,7 @@ document.getElementById("ValidationForm").addEventListener("submit", function(e)
             }
             console.log(this.tagName);
         })
- 
-        
 
+import {validateLength,validatepasswordLength,confirmPassword} from "./validate.js";
 
 
--- validate.js
+++ validate.js
@@ -1,27 +1,30 @@
-export function validateLength(element, minLength = 2) {
-    if (element.length < minLength)
-        document.getElementById(`error-${element.id}`).innerText = "Hej"
-    
+export  function validateLength (event, name, minLength = 2) {
+      const error = document.getElementById(`error-${event.target.id}`)
+
+    if (event.target.value === "")
+        error.innerText = `Fill in your ${name.toLowerCase()}.`
+
+    else
+      if (event.target.value.length < minLength)
+        error.innerText = `Your ${name.toLowerCase()} must be more than ${minLength} characters.`;
+      else
+      error.innerText = " "  
+
 }
 
-export function comparePassword(Pword, confirmPword) {
-  const error = document.getElementById(`error-${confirmPword.target.id}`);
+export function confirmPassword(password, confirmPassword, event, name) {
+  const error = document.getElementById(`error-${event.target.id}`);
 
-  if (Pword !== confirmPword.target.value)
-    error.innerText = `Password is incorrect`;
-  else error.innerText = "";
+  if (password.event.target.value !== confirmPassword.event.target.value)
+      error.innerText = `Your ${name} does not match `
 }
 
-export function validateEmail(element) {
-  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export function validatepasswordLength(event, name, minpassLength = 6) {
+  const error = document.getElementById(`error-${event.target.id}`);
 
-  if (!regEx.test(element.value)) {
-    document.getElementById(
-      `error-${element.id}`
-    ).innerText = `Your e-mail must be valid.`;
-    return false;
-  } else {
-    document.getElementById(`error-${element.id}`).innerText = ``;
-    return true;
-  }
+  if (event.target.value === "")
+    error.innerText = `Fill in your ${name.toLowerCase()}.`;
+  else if (event.target.value.length < minpassLength)
+    error.innerText = `Your ${name.toLowerCase()} must be more than ${minpassLength} characters.`;
+  else error.innerText = " ";
 }
